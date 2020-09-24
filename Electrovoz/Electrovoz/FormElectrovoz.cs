f90d25a02d545c06e5289587d7378318f6f70e9a
Владислав Арзамасов <vladislav.arzamasov@outlook.com>
2020-09-24 11:51:00 +0400

    ready

---
+++ Electrovoz/Electrovoz/FormElectrovoz.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Electrovoz
+{
+    public partial class FormElectrovoz : Form
+    {
+        private Electrovozzz electrvoz;
+        public FormElectrovoz()
+        {
+            InitializeComponent();
+        }
+        // Метод отрисовки электровоза
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxElektrovoz.Width, pictureBoxElektrovoz.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            electrvoz.DrawTransport(gr);
+            pictureBoxElektrovoz.Image = bmp;
+        }
+        // Обработка нажатия кнопки "Создать"
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            electrvoz = new Electrovozzz(rnd.Next(100, 300), rnd.Next(1000, 2000), 
+           Color.Green, Color.Yellow, true, true);
+            electrvoz.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxElektrovoz.Width,
+           pictureBoxElektrovoz.Height);
+            Draw();
+        }
+        // Обработка нажатия кнопок управления
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    electrvoz.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    electrvoz.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    electrvoz.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    electrvoz.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+    }
+}
